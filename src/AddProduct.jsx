723334f99356ab94a7631cdbf3c83800e7fa997e
alvin1904 <alvin19official@gmail.com>
2023-01-08 21:28:46 +0530

    complete most styling work

--- src/AddProduct.jsx
+++ src/AddProduct.jsx
@@ -1,8 +1,16 @@
 import { useState } from "react";
 import "./App.css";
 import ImageSelector from "./ImageSelector";
+import { FaRupeeSign } from "react-icons/fa";
 
 function AddProduct() {
+
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+  }
+
+
+
   return (
     <form className="form" action="submit">
       <p className="heading">Add a product</p>
@@ -21,14 +29,18 @@ function AddProduct() {
         <div className="section">
           <p className="subheading">Price</p>
           <section className="flex-row">
-            <input type="text" placeholder="Sale price"></input>
-            <input type="text" placeholder="Compare at price (will be striked off)"></input>
+            <div><FaRupeeSign/><input type="text" placeholder="Sale price"></input></div>
+            <div><FaRupeeSign className="red"/><input type="text" placeholder="Compare at price (will be striked off)" className="red"></input></div>
           </section>
         </div>
+        <br></br>
         <div className="section">
           <p className="subheading">Add media</p>
             4 images of 1:1 ratio recommended<br/>
           <ImageSelector />
+        </div>
+        <div className="section btn-section">
+          <button className="btn" type='submit' onClick={handleSubmit}>Add product</button>
         </div>
       </div>
     </form>
